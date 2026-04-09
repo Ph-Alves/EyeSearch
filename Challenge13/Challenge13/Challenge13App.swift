9914de2c32d60d85e065a3326a447a45093ec883
Ph-Alves <ph.costaalves@gmail.com>
2026-04-09 09:23:44 -0300

    Montando estrutura de pastas

---
+++ Challenge13/Challenge13/Challenge13App.swift
@@ -0,0 +1,33 @@
+//
+//  Challenge13App.swift
+//  Challenge13
+//
+//  Created by Paulo Henrique Costa Alves on 09/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Challenge13App: App {
+    // Config do banco de dados
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Challenge13/Challenge13/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Challenge13
+//
+//  Created by Paulo Henrique Costa Alves on 09/04/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
