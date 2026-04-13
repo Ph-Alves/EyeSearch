761beee6fda0db2e2f840b2c08453ba49fa5e775
Manoel Pedro <manoeltelesdev@gmail.com>
2026-04-13 14:54:50 -0300

    chore(AdesivoDetector)x: add MLModel

--- Challenge13/Challenge13/Managers/CameraManager.swift
+++ Challenge13/Challenge13/Managers/CameraManager.swift
@@ -17,8 +17,8 @@ class CameraManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     private(set) var session = AVCaptureSession()
     private let videoOutput = AVCaptureVideoDataOutput()
     
-//    let model = try? StickerDetector()
-//    var prediction: StickerDetectorOutput?
+    let model = try? AdesivoDetector()
+    var prediction: AdesivoDetectorOutput?
     
     //MARK: Functions
     @MainActor
@@ -77,7 +77,7 @@ class CameraManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
         else { return }
         
-//        self.prediction = try? model?.prediction(image: pixelBuffer)
+        self.prediction = try? model?.prediction(input: pixelBuffer)
         
             
     }
