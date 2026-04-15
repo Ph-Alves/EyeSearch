cd1fa4270aa19c692fcf380d7472d65d745777af
Manoel Pedro <manoeltelesdev@gmail.com>
2026-04-15 14:11:03 -0300

    feat(MLModelManager): add YOLOv3 model via LFS

---
+++ Challenge13/Challenge13/Managers/MLModelManager.swift
@@ -0,0 +1,17 @@
+//
+//  MLModelManager.swift
+//  EyeSearch
+//
+//  Created by Manoel Pedro Prado Sa Teles on 15/04/26.
+//
+
+import Foundation
+import CoreML
+
+@Observable
+class MLModelManager{
+    
+    
+    
+    
+}
