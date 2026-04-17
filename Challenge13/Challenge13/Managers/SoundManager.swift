00a9b3f08179b513f9e6b4d7b48a6386f825cc42
Raquel Souza <raquelpds09@gmail.com>
2026-04-17 11:28:57 -0300

    refactor(SoundManager): make SoundManager stateless and add protocol abstraction

--- Challenge13/Challenge13/Managers/SoundManager.swift
+++ Challenge13/Challenge13/Managers/SoundManager.swift
@@ -8,40 +8,33 @@
 import Foundation
 import AVFoundation
 
-// É um singleton, ou seja, só permite uma instância dele no projeto
-class SoundManager {
-    
+
+final class SoundManager: SoundManaging {
     
     // Essa é a instância, é static pois permite o uso sem precisar instanciar a classe antes, até por que ela também é uma instância.
     static let manager = SoundManager()
-    private var muted: Bool = false
     var player: AVAudioPlayer?
     
     // Init privado para proibir instanciação manual
     private init() {}
     
     // Função de tocar som a partir de um url (.mp3 do audio)
-    func playSound() {
-        if muted { return } else {
-            guard let url = Bundle.main.url(forResource: "item-found", withExtension: "mp3") else { return }
-            
-            do {
-                // instância do objeto audioPlayer
-                player = try AVAudioPlayer(contentsOf: url)
-                player?.setVolume(10, fadeDuration: 3)
-                player?.play()
-            } catch {
-                print("Error iniciating AVAudioPlayer: \(error.localizedDescription)")
-            }
+    func playSound(isEnabled: Bool) {
+        guard isEnabled else { return }
+        
+        guard let url = Bundle.main.url(forResource: "item-found", withExtension: "mp3") else { return }
+        
+        do {
+            // instância do objeto audioPlayer
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.setVolume(10, fadeDuration: 3)
+            player?.play()
+        } catch {
+            print("Error initiating AVAudioPlayer: \(error.localizedDescription)")
         }
     }
     
-    func toggleSound() {
-        self.muted.toggle()
-    }
-    
     func reset() {
-        self.muted = false
-        self.player = nil
-    }
+            player = nil
+        }
 }
